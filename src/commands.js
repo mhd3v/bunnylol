2f8cdc2294aee57bd0756613fb10cedb9a76da93
Mahad Aamir <iam@mhd3v.com>
2025-10-16 10:49:24 +0200

    Update commands

--- src/commands.js
+++ src/commands.js
@@ -21,53 +21,29 @@ export type ColumnDataTableType = {|
 |};
 
 export const COMMANDS: {[CommandNames] : CommandType} = {
-    fb: {
-        name: "Facebook",
-        url: "https://facebook.com/",
-        searchurl: "https://www.facebook.com/search/top/?q="
-    },
-    m: {
-        name: "Messenger Desktop App",
-        url: "messenger://",
-    },
-    mw: {
-        name: "Messenger Web",
-        url: "https://www.messenger.com/"
-    },
     wa: {
-        name: "WhatsApp Desktop App",
-        url: "whatsapp://",
-    },
-    waw: {
         name: "WhatsApp Web",
         url: "https://web.whatsapp.com/"
     },
     gm: {
         name: "Gmail",
         url: "https://mail.google.com/mail/u/0",
-        searchurl: "https://mail.google.com/mail/u/"
+        searchurl: "https://mail.google.com/mail/u/0/#search/q="
     },
     gd: {
         name: "Google Drive",
         url: "https://drive.google.com/drive/u/0",
         searchurl: "https://drive.google.com/drive/u/"
     },
-    sis: {
-        name: "UVA SIS",
-        url: "https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_GN.H_SPRINGBOARD.FieldFormula.IScript_Main",
-    },
-    col: {
-        name: "UVA Collab",
-        url: "https://collab.its.virginia.edu/portal",
+    docs: {
+        name: "Google Drive",
+        url: "https://docs.google.com/",
+        searchurl: "https://docs.google.com/document/u/0/?search_query="
     },
     yt: {
         name: "YouTube",
         url: "https://youtube.com/",
         searchurl: "https://www.youtube.com/results?search_query="
-    },
-    tv: {
-        name: "YouTube TV",
-        url: "https://tv.youtube.com/",
     },
     gh: {
         name: "GitHub",
@@ -94,86 +70,29 @@ export const COMMANDS: {[CommandNames] : CommandType} = {
         searchurl: "https://twitter.com/search?q="
     },
     me: {
-        name: "Rithik.me - Personal Website",
-        url: "https://rithik.me/me",
+        name: "Mahad Aamir - Personal Website",
+        url: "https://mhd3v.com/",
     },
     g: {
         name: "Google",
         url: "https://google.com/",
         searchurl: "https://www.google.com/search?q="
     },
-    wp: {
-        name: "Washington Post",
-        url: "https://www.washingtonpost.com/regional/"
-    },
-    wsj: {
-        name: "Wall Street Journal",
-        url: "https://www.wsj.com/"
-    },
-    cnn: {
-        name: "CNN",
-        url: "https://www.cnn.com/"
-    },
-    tr: {
-        name: "HooHacks Trello Board",
-        url: "https://trello.com/b/GjKhtVPK/hoohacks"
-    },
-    n: {
-        name: "Netflix",
-        url: "https://netflix.com/",
-        searchurl: "https://www.netflix.com/search?q="
-    },
-    h: {
-        name: "Hulu",
-        url: "https://hulu.com/",
-    },
-    pv: {
-        name: "Amazon Prime Video",
-        url: "https://www.amazon.com/Amazon-Video/b/?&node=2858778011&ref=dvm_MLP_ROWNA_US_1",
-        searchurl: "https://www.amazon.com/s?i=instant-video&ref=nb_sb_noss_2&k="
-    },
-    p: {
-        name: "Piazza",
-        url: "https://piazza.com/class"
-    },
     vs: {
         name: "VS Code",
         url: "vscode://",
     },
-    wf: {
-        name: "Webflow",
-        url: "https://webflow.com/design/hoohacks"
+    code: {
+        name: "VS Code",
+        url: "vscode://",
     },
-    hs: {
-        name: "Hubspot",
-        url: "https://app.hubspot.com/"
-    },
-    $: {
-        name: "Robinhood",
-        url: "https://robinhood.com/", 
-        searchurl: "https://robinhood.com/stocks/"
-    },
-    c: {
-        name: "Robinhood Crypto",
-        url: "https://robinhood.com/", 
-        searchurl: "https://robinhood.com/crypto/"
+    slack: {
+        name: "Slack",
+        url: "slack://",
     },
     cal: {
         name: "Google Calendar",
         url: "https://calendar.google.com/calendar/r"
-    },
-    uvacovid: {
-        name: "UVA COVID-19 Tracker",
-        url: "https://returntogrounds.virginia.edu/covid-tracker"
-    },
-    wiki: {
-        name: "Wikipedia",
-        url: "https://en.wikipedia.org",
-        searchurl: "https://en.wikipedia.org/wiki/"
-    },
-    todo: {
-        name: "Microsoft To Do",
-        url: "https://to-do.live.com"
     },
     DEFAULT: {
         name: "Default - Google Search",
